e98b6122c03ea495a0db7b8cc8bd3fcbf5388169
Akash Bhagat <bhagatakash0123@gmail.com>
2024-02-27 15:39:13 +0530

    done with cmptrie

--- main.c
+++ main.c
@@ -3,25 +3,6 @@
 #include "intr/cmptrie.h"
 #include "intr/rfd_utils.h"
 
-long npfor(refdata_info *rfd, long parentnp, char forchar);
-
-int get_meaning(long mp, char *meaning, refdata_info *rfd)
-{
-    uchar len ;
-    FILE *fp;
-
-    if(mp && ((fp= fopen("data/len_meaning", "rb") ) != NULL))
-    {
-        fseek(fp, mp, SEEK_SET);
-        fread(&len, 1, 1, fp);
-        fread(meaning, 1, len, fp);
-        meaning[len] = '\0';
-        fclose(fp);
-    }
-    
-    return (mp != 0);
-}
-
 int search_words(char *filename, refdata_info *rfd)
 {
     char word[100], meaning[1000];
@@ -34,12 +15,9 @@ int search_words(char *filename, refdata_info *rfd)
         nwc = 0, cwc = 0;
         while(fscanf(fp, "%s\n", word) != EOF)
         {
-            cwc += ((mp = is_word_present(rfd, word)) != 0);
-            get_meaning(mp, meaning, rfd);
-
-            printf("%s\n", meaning);
+            if(get_meaning(rfd, word, meaning))
+                printf("%s\n", meaning);
         }
-        printf("wc (compressed trie search) : %d\n", cwc);
         fclose(fp);
     }
     return nwc;
--- src/cmptrie.c
+++ src/cmptrie.c
@@ -6,6 +6,13 @@
 #include "bitsarr.h"
 #include "./debug.c"
 
+#define LEN_MEANING_FILE "data/len_meaning"
+
+long npfor(refdata_info *rfd, long parentnp, char nextchar);
+long get_mmap(refdata_info *rfd, long np, int *map);
+
+
+// initilize the refdata_info object with data read from rfd_file reference
 int init_refdata_info(char *rfd_file, refdata_info **rfd)
 {
     int res = 0;
@@ -41,6 +48,7 @@ int init_refdata_info(char *rfd_file, refdata_info **rfd)
     return res;
 }
 
+// frees refdata_info object
 int free_refdata_info(refdata_info *rfd)
 {
     if(rfd)
@@ -51,13 +59,62 @@ int free_refdata_info(refdata_info *rfd)
 }
 
 
-long npfor(refdata_info *rfd, long parentnp, char forchar)
+long is_word_present(refdata_info *rfd, char *word)
+{
+
+    long np = rfd->rootsbit, mp = 0;
+    int i = 0,  map = 0;
+    while((word[i+1] != '\0') && (np != -1))
+        np = npfor(rfd, np, word[i++]);
+
+    if(np != -1)
+    {
+        char ch = word[i] - 'a'; 
+        np = get_mmap(rfd, np, &map);
+        mp = ((map & (1<<ch))>>ch);
+        for(int i = 0; (i < ch) && mp; i++)
+            if(map & (1<<i))       
+                np += rfd->mpsize;
+        if(mp)
+        {
+            mp *= 0;
+            bitscopy(rfd->refdata, np, &mp, 0, rfd->mpsize);
+        }
+    }
+    return mp;
+}
+
+int get_meaning(refdata_info *rfd, char *word, char *meaning)
+{
+    uchar len ;
+    FILE *fp;
+    long mp = is_word_present(rfd, word);
+    
+    if(mp && ((fp= fopen(LEN_MEANING_FILE, "rb") ) != NULL))
+    {
+        fseek(fp, mp, SEEK_SET);
+        fread(&len, 1, 1, fp);
+        fread(meaning, 1, len, fp);
+        meaning[len] = '\0';
+        fclose(fp);
+    }
+    else
+        mp = 0; 
+    return (mp != 0);
+}
+
+// **** internal methods use to traverse & search for word in rfd ***********
+
+// get next pointer for char nextchar at current node given by parentnp
+// if next node found for nextchar then returns pointer (bit level) for that node
+// else return -1
+long npfor(refdata_info *rfd, long parentnp, char nextchar)
 {
     char ch1, ch2;
     enum Type tp = 0;
     long curpos = parentnp, nextnp = -1;
 
-    forchar -= 'a';
+    nextchar -= 'a';
     bitscopy(rfd->refdata, curpos, &tp, 0, 3);
     curpos += 3;
 
@@ -69,7 +126,7 @@ long npfor(refdata_info *rfd, long parentnp, char forchar)
             bitscopy(rfd->refdata, curpos, &ch1, 0, 5);
             curpos += 5;
 
-            if(ch1 == forchar)
+            if(ch1 == nextchar)
             {
                 nextnp *= 0;
                 bitscopy(rfd->refdata, curpos, &nextnp, 0, rfd->npsize);
@@ -85,13 +142,13 @@ long npfor(refdata_info *rfd, long parentnp, char forchar)
             bitscopy(rfd->refdata, curpos, &ch2, 0, 5);
             curpos += 5;
 
-            if(ch1 == forchar)
+            if(ch1 == nextchar)
             {
                 nextnp *= 0;
                 bitscopy(rfd->refdata, curpos, &nextnp, 0, rfd->npsize);
                 curpos += rfd->npsize;
             }
-            else if(ch2 == forchar)
+            else if(ch2 == nextchar)
             {
                 nextnp *= 0;
                 curpos += rfd->npsize;
@@ -100,7 +157,7 @@ long npfor(refdata_info *rfd, long parentnp, char forchar)
             break;
 
         case FULL_T:
-            curpos += (rfd->npsize *  forchar);
+            curpos += (rfd->npsize *  nextchar);
             nextnp = 0;
             
             bitscopy(rfd->refdata, curpos, &nextnp, 0, rfd->npsize);
@@ -111,9 +168,9 @@ long npfor(refdata_info *rfd, long parentnp, char forchar)
         bitscopy(rfd->refdata, curpos, &npmap, 0, NCHRS);
         curpos += NCHRS;
 
-        if(npmap & (1<<forchar))
+        if(npmap & (1<<nextchar))
         {
-            for(int i = 0; i < forchar; i++)
+            for(int i = 0; i < nextchar; i++)
                 if(npmap & (1<<i))
                     curpos += rfd->npsize;
 
@@ -126,6 +183,7 @@ long npfor(refdata_info *rfd, long parentnp, char forchar)
 }
 
 
+// retrives meaning bitmap to map from node np
 long get_mmap(refdata_info *rfd, long np, int *map)
 {
     enum Type tp = 0;
